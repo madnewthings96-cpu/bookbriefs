1dd7f4431fe0213e93f32687ba16e508f7e91185
HVMZA <belhal@HVMZAs-Mac-mini.local>
2025-09-16 20:31:24 +0100

    refactor: move banner image from footer to hero section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,15 +5,6 @@ const Footer: React.FC = () => {
 
   return (
     <footer className="bg-slate-800 text-gray-300" style={{ backgroundColor: '#2F4F4F' }}>
-      {/* Banner Image */}
-      <div className="w-full">
-        <img 
-          src="/images/Website banner bookbriefs 5.png" 
-          alt="BookBriefs Banner" 
-          className="w-full h-auto object-cover"
-        />
-      </div>
-      
       <div className="container mx-auto py-8 px-4 sm:px-6 lg:px-8">
         {/* Logo Section */}
         <div className="text-center mb-8">
